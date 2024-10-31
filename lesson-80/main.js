84ef30ae7b5fc6e1b979bd6776b5cb0d0958d759
AleksandrIl111 <Aleksandr1.78@inbox.ru>
2024-10-31 22:37:45 +0800

    Add files via upload

---
+++ lesson-80/main.js
@@ -0,0 +1,23 @@
+for (let i = 2; i <= 12; i++) {
+    if (i % 2 === 0) {
+        console.log(i);
+    }
+}
+
+for (let j = 10; j >= 1; j--) {
+    console.log(j);
+}
+
+let i = 2;
+while (i <= 12) {
+    if (i % 2 === 0) {
+        console.log(i);
+    }
+    i++;
+}
+
+let j = 10;
+while (j >= 1) {
+    console.log(j);
+    j--;
+}
